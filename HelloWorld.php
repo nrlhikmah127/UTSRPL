609752a619e709471f4a2ac9c230a8557a87f20a
Nurul Hikmah <nrlhikmah127@gmail.com>
2021-09-27 08:37:30 +0800

    menambahkan tanggal pada file

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UTS RPL</title>
-</head>
-<body>
-  <p>Nurul Hikmah</p>
-  <p>1810817220022</p>
-  <p>Hello World, UTS RPL 2021</p>
-</body>
-</html>
+<?php 
+echo "Nurul Hikmah";
+echo "1810817220022";
+echo "Hello World, UTS RPL 2021";
+echo "Senin, 27 September 2021";
